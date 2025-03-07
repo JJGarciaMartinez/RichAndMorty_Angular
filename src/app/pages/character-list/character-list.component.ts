b840eabfb099c704ea4580a8e7bed56c1e86b04f
JJGarciaMartinez <juangm2302@gmail.com>
2025-03-07 17:01:12 -0600

    Change cards distribution

--- src/app/pages/character-list/character-list.component.ts
+++ src/app/pages/character-list/character-list.component.ts
@@ -3,10 +3,19 @@ import { RickAndMortyService } from '@services/rick-n-morty.service';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from '@components/pagination/pagination.component';
+import { SpinnerComponent, TooltipDirective } from '@coreui/angular';
+import { IconComponent } from '@components/icon/icon.component';
 
 @Component({
   selector: 'app-character-list',
-  imports: [RouterModule, CommonModule, PaginationComponent],
+  imports: [
+    RouterModule,
+    CommonModule,
+    PaginationComponent,
+    SpinnerComponent,
+    IconComponent,
+    TooltipDirective,
+  ],
   templateUrl: './character-list.component.html',
   styleUrl: './character-list.component.css',
 })
@@ -43,7 +52,10 @@ export class CharacterListComponent {
     this.rickAndMortyService
       .getCharactersByPage(pageQuery)
       .subscribe((data) => {
-        this.characters = data.results;
+        this.characters = data.results.map((character: any) => ({
+          ...character,
+          loading: true,
+        }));
         this.info = data.info;
         this.listPages = Array.from(
           { length: this.info.pages },
@@ -60,7 +72,10 @@ export class CharacterListComponent {
     this.isLoading = true;
     try {
       const data = await this.rickAndMortyService.getCharacters().toPromise();
-      this.characters = data.results;
+      this.characters = data.results.map((character: any) => ({
+        ...character,
+        loading: true,
+      }));
       this.info = data.info;
       this.listPages = Array.from({ length: this.info.pages }, (_, i) => i + 1);
     } catch (error) {
@@ -73,7 +88,10 @@ export class CharacterListComponent {
   // Function to handle the page change event
   goToPage(page: number) {
     this.rickAndMortyService.getCharactersByPage(page).subscribe((data) => {
-      this.characters = data.results;
+      this.characters = data.results.map((character: any) => ({
+        ...character,
+        loading: true,
+      }));
       this.info = data.info;
     });
 
@@ -88,5 +106,34 @@ export class CharacterListComponent {
     });
 
     this.router.navigateByUrl(url);
+  }
+
+  onImageLoad(item: any): void {
+    item.loading = false;
+  }
+
+  async searchCharacter(nameQuery: string, statusQuery: string) {
+    await this.searchCharacterOrStatus(nameQuery, statusQuery);
+  }
+  async searchCharacterOrStatus(name: string, status: string) {
+    this.isLoading = true;
+    this.rickAndMortyService.getCharacterByNameOrStatus(name, status).subscribe(
+      (data) => {
+        this.characters = data.results.map((character: any) => ({
+          ...character,
+          loading: true,
+        }));
+        this.info = data.info;
+        this.listPages = Array.from(
+          { length: this.info.pages },
+          (_, i) => i + 1
+        );
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error fetching characters:', error);
+        this.isLoading = false;
+      }
+    );
   }
 }
